2d33a73bdde1303a612b2e48dc7dee070694409e
Thapa <thapak@cromwell.co.uk>
2020-01-07 16:33:43 +0000

    Files updated

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Post;
+use App\Setting;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class FrontendController extends Controller
+{
+    public function index() {
+        return view('index')
+            ->with([
+                'title' => Setting::first()->site_name,
+                'categories' => Category::take(5)->get(),
+                'first_post' => Post::orderBy('created_at', 'desc')->first(),
+                'second_post' => Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first(),
+                'third_post' => Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first()
+            ])
+            ;
+    }
+}
